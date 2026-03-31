1e603f59ae351e3a8590d7056b3dd80c917bf1b9
Hrach Daniyl <daniel.grach06@gmail.com>
2026-03-31 19:50:14 +0300

    Initial commit

---
+++ Lab11-OOP/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Lab11_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtDeptID = new System.Windows.Forms.TextBox();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridViewReports = new System.Windows.Forms.DataGridView();
+            this.btnReport1 = new System.Windows.Forms.Button();
+            this.btnReport2 = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReports)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(12, 68);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(220, 22);
+            this.txtFullName.TabIndex = 0;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(251, 68);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(178, 22);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // txtDeptID
+            // 
+            this.txtDeptID.Location = new System.Drawing.Point(471, 68);
+            this.txtDeptID.Name = "txtDeptID";
+            this.txtDeptID.Size = new System.Drawing.Size(100, 22);
+            this.txtDeptID.TabIndex = 2;
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.Location = new System.Drawing.Point(12, 96);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(75, 23);
+            this.btnAddEmployee.TabIndex = 3;
+            this.btnAddEmployee.Text = "Додати";
+            this.btnAddEmployee.UseVisualStyleBackColor = true;
+            this.btnAddEmployee.Click += new System.EventHandler(this.btnAddEmployee_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ПІБ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Номер тел.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Номер відділу:";
+            // 
+            // dataGridViewReports
+            // 
+            this.dataGridViewReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewReports.Location = new System.Drawing.Point(15, 134);
+            this.dataGridViewReports.Name = "dataGridViewReports";
+            this.dataGridViewReports.RowHeadersWidth = 51;
+            this.dataGridViewReports.RowTemplate.Height = 24;
+            this.dataGridViewReports.Size = new System.Drawing.Size(559, 200);
+            this.dataGridViewReports.TabIndex = 7;
+            // 
+            // btnReport1
+            // 
+            this.btnReport1.Location = new System.Drawing.Point(46, 358);
+            this.btnReport1.Name = "btnReport1";
+            this.btnReport1.Size = new System.Drawing.Size(196, 23);
+            this.btnReport1.TabIndex = 8;
+            this.btnReport1.Text = "Звіт: Працівники і відділи";
+            this.btnReport1.UseVisualStyleBackColor = true;
+            this.btnReport1.Click += new System.EventHandler(this.btnReport1_Click);
+            // 
+            // btnReport2
+            // 
+            this.btnReport2.Location = new System.Drawing.Point(46, 387);
+            this.btnReport2.Name = "btnReport2";
+            this.btnReport2.Size = new System.Drawing.Size(196, 23);
+            this.btnReport2.TabIndex = 9;
+            this.btnReport2.Text = "Звіт: Кількість по відділах";
+            this.btnReport2.UseVisualStyleBackColor = true;
+            this.btnReport2.Click += new System.EventHandler(this.btnReport2_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(402, 388);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(119, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Знайти за ПІБ";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(350, 359);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(224, 22);
+            this.txtSearch.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Пошук:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnReport2);
+            this.Controls.Add(this.btnReport1);
+            this.Controls.Add(this.dataGridViewReports);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Controls.Add(this.txtDeptID);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtFullName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReports)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtDeptID;
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridViewReports;
+        private System.Windows.Forms.Button btnReport1;
+        private System.Windows.Forms.Button btnReport2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab11-OOP/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; // Бібліотека для роботи з базами даних OLE DB (включаючи MS Access)
+
+namespace Lab11_OOP
+{
+    public partial class Form1 : Form
+    {
+        // Підключення БД. 
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=HR_Database.accdb;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Універсальний допоміжний метод для виконання SELECT-запитів 
+        // та виведення отриманого результату у таблицю DataGridView
+        private void LoadDataToGrid(string query)
+        {
+            try
+            {
+                // Використовуємо блок using для автоматичного закриття підключення після завершення роботи з БД
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    // OleDbDataAdapter - це "міст" між базою даних та нашою програмою. Він отримує та виконує запит.
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+
+                    // DataTable - це віртуальна таблиця в оперативній пам'яті, куди ми завантажимо дані
+                    DataTable table = new DataTable();
+
+                    // Заповнюємо таблицю результатами виконання SQL-запиту
+                    adapter.Fill(table);
+
+                    // Прив'язуємо заповнену таблицю до візуального компонента DataGridView на формі
+                    dataGridViewReports.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Якщо щось піде не так (наприклад, файл БД не знайдено або помилка в SQL), покажемо вікно з помилкою
+                MessageBox.Show("Помилка роботи з базою даних: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Введення даних у БД (Додавання нового співробітника)
+        private void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            // SQL-запит на додавання (INSERT). 
+            // @name, @phone, @dept - це параметри. Їх використання захищає базу від SQL-ін'єкцій та помилок синтаксису (наприклад, якщо в імені є апостроф).
+            string query = "INSERT INTO Employees (FullName, Phone, DeptID) VALUES (@name, @phone, @dept)";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                // Створюємо об'єкт команди, який безпосередньо виконає наш запит у базі
+                OleDbCommand command = new OleDbCommand(query, connection);
+
+                // Підставляємо реальні значення з текстових полів (TextBox) на формі замість параметрів у запиті
+                command.Parameters.AddWithValue("@name", txtFullName.Text);
+                command.Parameters.AddWithValue("@phone", txtPhone.Text);
+
+                // Перетворюємо текст на число (Int32), оскільки поле DeptID у базі має числовий тип
+                command.Parameters.AddWithValue("@dept", Convert.ToInt32(txtDeptID.Text));
+
+                try
+                {
+                    // Відкриваємо фізичне підключення до бази даних
+                    connection.Open();
+
+                    // Виконуємо запит, який модифікує дані, але не повертає таблицю (INSERT, UPDATE, DELETE)
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Співробітника успішно додано!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Очищуємо поля введення на формі, щоб підготувати їх для наступного співробітника
+                    txtFullName.Clear();
+                    txtPhone.Clear();
+                    txtDeptID.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка при додаванні: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Побудова Звіту 1 (Список співробітників + Назва їх відділу)
+        private void btnReport1_Click(object sender, EventArgs e)
+        {
+            // Запит з використанням INNER JOIN. 
+            // Він об'єднує дві таблиці (Employees та Departments) за їхнім спільним полем (DeptID).
+            // Ключове слово AS дозволяє задати красиві назви (псевдоніми) для стовпчиків, які побачить користувач.
+            string query = @"SELECT Employees.FullName AS [ПІБ], 
+                                    Employees.Phone AS [Телефон], 
+                                    Departments.DeptName AS [Назва відділу]
+                             FROM Employees 
+                             INNER JOIN Departments ON Employees.DeptID = Departments.DeptID";
+
+            // Викликаємо наш універсальний метод для виконання запиту та виведення результату
+            LoadDataToGrid(query);
+        }
+
+        // Побудова Звіту 2 (Кількість працівників у кожному відділі)
+        private void btnReport2_Click(object sender, EventArgs e)
+        {
+            // Запит з використанням агрегатної функції COUNT (підрахунок) та групування GROUP BY
+            string query = @"SELECT Departments.DeptName AS [Відділ], 
+                                    COUNT(Employees.EmpID) AS [Кількість працівників]
+                             FROM Departments 
+                             LEFT JOIN Employees ON Departments.DeptID = Employees.DeptID 
+                             GROUP BY Departments.DeptName";
+
+            LoadDataToGrid(query);
+        }
+
+        // Пошук у БД по одному критерію (За ПІБ або частиною ПІБ)
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Використовуємо оператор LIKE для пошуку за шаблоном (не точний збіг, а входження тексту)
+            string query = "SELECT * FROM Employees WHERE FullName LIKE @search";
+
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+
+                    // Підставляємо параметр для пошуку. 
+                    // Знак % означає "будь-яка кількість будь-яких символів". 
+                    // Тобто, якщо користувач введе "Іван", програма шукатиме "%Іван%" (Іван може бути на початку, в середині або в кінці ПІБ).
+                    adapter.SelectCommand.Parameters.AddWithValue("@search", "%" + txtSearch.Text + "%");
+
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // Виводимо знайдені дані у таблицю
+                    dataGridViewReports.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка пошуку: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
